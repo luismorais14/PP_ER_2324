73fd502b6cabb086d7da58f4932e5e95827caeee
Asus <Asus@LAPTOP-UGFHQ1JJ.lan>
2024-06-23 17:07:02 +0100

    Acabar os métodos que faltam em Institution, verifica os que fiz e vÊ os comentários

--- src/project/InstitutionImpl.java
+++ src/project/InstitutionImpl.java
@@ -24,6 +24,10 @@ import com.estg.pickingManagement.PickingMap;
 import com.estg.pickingManagement.Vehicle;
 
 import java.time.LocalDateTime;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class InstitutionImpl implements Institution {
 
@@ -97,7 +101,8 @@ public class InstitutionImpl implements Institution {
     /**
      * Define os picking maps da instituição.
      *
-     * @param pickingMaps os novos mapas de picking a serem atribuídos à instituição
+     * @param pickingMaps os novos mapas de picking a serem atribuídos à
+     * instituição
      */
     public void setPickingMaps(PickingMap[] pickingMaps) {
         this.pickingMaps = pickingMaps;
@@ -124,7 +129,8 @@ public class InstitutionImpl implements Institution {
     /**
      * Define os veículos habilitados associados à instituição.
      *
-     * @param vehicle os novos veículos habilitados a serem atribuídos à instituição
+     * @param vehicle os novos veículos habilitados a serem atribuídos à
+     * instituição
      */
     public void setVehicle(Vehicle[] vehicle) {
         this.enableVehicles = vehicle;
@@ -145,7 +151,8 @@ public class InstitutionImpl implements Institution {
      *
      * @param aidbox a aidbox a ser adicionada à instituição
      * @return true se a aidbox foi adicionada com sucesso, false caso contrário
-     * @throws AidBoxException se a caixa de ajuda for nula ou contiver containers duplicados de um determinado tipo
+     * @throws AidBoxException se a caixa de ajuda for nula ou contiver
+     * containers duplicados de um determinado tipo
      */
     @Override
     public boolean addAidBox(AidBox aidbox) throws AidBoxException {
@@ -162,7 +169,8 @@ public class InstitutionImpl implements Institution {
      *
      * @param msrmnt a medição a ser adicionada
      * @param cntnr o contêiner ao qual a medição será adicionada
-     * @return true se a medição foi adicionada com sucesso, false caso contrário
+     * @return true se a medição foi adicionada com sucesso, false caso
+     * contrário
      * @throws ContainerException se o contêiner for nulo
      * @throws MeasurementException se o valor da medição for inválido
      */
@@ -178,9 +186,14 @@ public class InstitutionImpl implements Institution {
         return this.measurements.addMeasurement(msrmnt);
     }
 
+    /**
+     * Obtém uma cópia profunda das caixas de ajuda associadas à instituição.
+     *
+     * @return uma cópia profunda das caixas de ajuda associadas à instituição
+     */
     @Override
     public AidBox[] getAidBoxes() {
-        return this.deepCopyAidBoxes(this.aidboxes.getAidBoxes());
+        return deepCopyAidBoxes(this.aidboxes.getAidBoxes()); // Verifica se esta correto
     }
 
     @Override
@@ -193,9 +206,39 @@ public class InstitutionImpl implements Institution {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    /**
+     * Adiciona um veículo habilitado à instituição.
+     *
+     * @param vhcl o veículo a ser adicionado à instituição
+     * @return true se o veículo foi adicionado com sucesso, false caso
+     * contrário
+     * @throws VehicleException se o veículo for nulo
+     */
     @Override
     public boolean addVehicle(Vehicle vhcl) throws VehicleException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        boolean aux = true;
+        if (vhcl == null) {
+            throw new VehicleException("Vehicle is null.");
+        }
+
+        if (enableVehicles == null) {
+            enableVehicles = new Vehicle[10];
+            nEnableVehicles = 0;
+        }
+
+        if (nEnableVehicles >= enableVehicles.length) {
+            expandVehicleArray();
+        }
+
+        for (Vehicle veh : enableVehicles) {
+            if (veh != null && veh.equals(vhcl)) {
+                aux = false;
+            }
+        }
+
+        enableVehicles[nEnableVehicles] = vhcl;
+        nEnableVehicles++;
+        return aux;
     }
 
     @Override
@@ -208,33 +251,126 @@ public class InstitutionImpl implements Institution {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    /**
+     * Obtém as mapas de picking associadas à instituição.
+     *
+     * @return os mapas de picking associados à instituição
+     */
     @Override
     public PickingMap[] getPickingMaps() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public PickingMap[] getPickingMaps(LocalDateTime ldt, LocalDateTime ldt1) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public PickingMap getCurrentPickingMap() throws PickingMapException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public boolean addPickingMap(PickingMap pm) throws PickingMapException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public double getDistance(AidBox aidbox) throws AidBoxException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return this.pickingMaps;
     }
 
     /**
-     * Método responsável por retornar a posição na coleção da aidbox com o container recebido como parâmetro
+     * Obtém os mapas de picking associados à instituição dentro de um intervalo
+     * de datas especificado.
+     *
+     * @param ldt a data de início do intervalo de datas
+     * @param ldt1 a data de término do intervalo de datas
+     * @return os mapas de picking associados à instituição dentro do intervalo
+     * de datas especificado
+     */
+    @Override
+    public PickingMap[] getPickingMaps(LocalDateTime ldt, LocalDateTime ldt1) {
+        PickingMap[] tmpPickingMap = new PickingMap[this.pickingMaps.length];
+        int tmpNPickingMaps = 0;
+        for (int i = 0; i < pickingMaps.length; i++) {
+            if (this.pickingMaps[i].getDate().isBefore(ldt1) && this.pickingMaps[i].getDate().isAfter(ldt)) {
+                tmpPickingMap[tmpNPickingMaps] = pickingMaps[i];
+                tmpNPickingMaps++;
+            }
+        }
+        return tmpPickingMap;
+    }
+
+    /**
+     * Obtém o mapa de picking atual da instituição.
+     *
+     * @return o mapa de picking atual da instituição
+     * @throws PickingMapException se não houver mapas de picking na instituição
+     */
+    @Override
+    public PickingMap getCurrentPickingMap() throws PickingMapException {
+        PickingMap pm = null;
+        if (this.pickingMaps.length == 0) {
+            throw new PickingMapException("There are no picking maps in the institution.");
+        }
+        for (int i = 0; i < pickingMaps.length; i++) {
+            pm = compareDate(this.pickingMaps[i].getDate());
+        }
+        return pm;
+    }
+
+    /**
+     * Adiciona um mapa de coleta à lista de mapas de coleta.
+     *
+     * @param pm O mapa de coleta a ser adicionado.
+     * @return true se o mapa de coleta foi adicionado com sucesso, false caso
+     * contrário.
+     * @throws PickingMapException Se o mapa de coleta fornecido for nulo.
+     */
+    @Override
+    public boolean addPickingMap(PickingMap pm) throws PickingMapException {
+        if (pm == null) {
+            throw new PickingMapException("The picking map is null.");
+        }
+        boolean aux = false;
+        if (!verifyPickingMapExistence(pm)) {
+            this.pickingMaps[nPickingMaps] = pm;
+            aux = true;
+            nPickingMaps++;
+        }
+        return aux;
+    }
+
+    /**
+     * Metodo responsável por retornar a distância entre a instituição atual e o
+     * parâmetro aidbox
+     *
+     * @param aidbox aidbox para calcular a distância
+     * @return a distância entre a instituição atual e a aidbox recebida como
+     * parâmetro
+     * @throws AidBoxException exceção a ser lançada se a aidbox não existir
+     */
+    @Override
+    public double getDistance(AidBox aidbox) throws AidBoxException {
+        if (aidbox == null) {
+            throw new AidBoxException("AidBox is null");
+        }
+
+        JSONArray ja;
+        JSONParser parser = new JSONParser();
+        String jsonString = this.httpProvider.getFromURL("https://data.mongodb-api.com/app/data-docuz/endpoint/distances");
+        double distance = 0.0;
+        try {
+            ja = (JSONArray) parser.parse(jsonString);
+            for (Object obj : ja) {
+                JSONObject jsonObject = (JSONObject) obj;
+                String originalAidBox = (String) jsonObject.get("from");
+                if (originalAidBox.compareTo(this.InstitutionName) == 0) { // Troquei o getCode por this.Instituition name para calcular a distancia da instituicao á aidbox
+                    JSONArray aidboxes = (JSONArray) jsonObject.get("to");
+                    for (Object aidboxesObj : aidboxes) {
+                        JSONObject aidboxObject = (JSONObject) aidboxesObj;
+                        String aidboxCode = (String) aidboxObject.get("name");
+                        if (aidboxCode.compareTo(aidbox.getCode()) == 0) {
+                            Object distanceObj = aidboxObject.get("distance");
+                            if (distanceObj instanceof Number) {
+                                distance = ((Number) distanceObj).doubleValue();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (ParseException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return distance;
+    }
+
+    /**
+     * Método responsável por retornar a posição na coleção da aidbox com o
+     * container recebido como parâmetro
+     *
      * @param cntnr
      * @return
      */
@@ -278,4 +414,57 @@ public class InstitutionImpl implements Institution {
         return newAidBox;
     }
 
+    /**
+     * Compara a data fornecida com as datas dos mapas de coleta e retorna o
+     * mapa de coleta mais próximo da data fornecida.
+     *
+     * @param dt a data a ser comparada
+     * @return o mapa de coleta mais próximo da data fornecida
+     */
+    private PickingMap compareDate(LocalDateTime dt) {
+        LocalDateTime aux = this.pickingMaps[0].getDate();
+        int index = 0;
+        for (int i = 0; i < pickingMaps.length; i++) {
+            if (this.pickingMaps[i].getDate().compareTo(dt) > 0) { // Retorna 0 se for igual/ Data anterior retorna número menor que 0/ Data maior retorna valor maior que 0
+                aux = this.pickingMaps[i].getDate();
+                index = i;
+            }
+        }
+        return this.pickingMaps[index];
+    }
+
+    /**
+     * Verifica a existência de uma rota de coleta em um mapa de coleta com base
+     * nas rotas de caixas de ajuda associadas.
+     *
+     * @param pm o mapa de coleta a ser verificado
+     * @return true se uma rota de coleta com base nas rotas de caixas de ajuda
+     * associadas já existir no mapa de coleta, caso contrário, false
+     */
+    private boolean verifyPickingMapExistence(PickingMap pm) {
+        boolean aux = false;
+        for (int i = 0; i < this.pickingMaps.length; i++) {
+            for (int j = 0; j < this.pickingMaps[i].getRoutes().length; j++) {
+                if (this.pickingMaps[i].getRoutes()[j].containsAidBox(pm.getRoutes()[i].getRoute()[j])) {
+                    aux = true;
+                }
+            }
+        }
+        return aux;
+    }
+
+        /**
+     * Expande a capacidade do array de veículos habilitados.
+     *
+     */
+    public void expandVehicleArray() {
+        Vehicle[] newArray = new Vehicle[ARRAY_SIZE + 5];
+        for (int i = 0; i < nEnableVehicles; i++) {
+            newArray[i] = enableVehicles[i];
+        }
+        this.enableVehicles = newArray;
+    }
+
+    
+    
 }
