3a0038880f983b4cbea485a74644e91edfcada46
Asus <Asus@LAPTOP-UGFHQ1JJ.lan>
2024-06-28 00:33:59 +0100

    erro faltava contador

--- src/management/VehicleManagement.java
+++ src/management/VehicleManagement.java
@@ -115,6 +115,7 @@ public class VehicleManagement {
         }
         if (index == nVehicles) {
             this.vehicles[index] = null;
+            nVehicles--; // Faltava
         } else {
             for (int i = index; i < nVehicles - index - 1; i++) {
                 this.vehicles[i] = this.vehicles[i + 1];
