713a3a6b2312f86a43d2003fb9aabbbbb27e34e8
Luís Morais <Lus Morais@192.168.1.130>
2024-06-30 16:29:34 +0100

    Adicionado cometários no início dos ficheiros que faltavam

--- src/management/PickingMapManagement.java
+++ src/management/PickingMapManagement.java
@@ -1,3 +1,12 @@
+/*
+ * Nome: Francisco Morais de Oliveira
+ * Número: 8230204
+ * Turma: T3
+ *
+ * Nome: Luís André Nunes Morais
+ * Número: 8230258
+ * Turma: T3
+ */
 package management;
 
 import com.estg.core.Container;
